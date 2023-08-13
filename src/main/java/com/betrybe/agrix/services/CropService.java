e9aa17fd54ac82fca73078b9adfafb408be51377
Bete.mrt <bete.mrt@gmail.com>
2023-08-13 17:53:33 -0300

    services

---
+++ src/main/java/com/betrybe/agrix/services/CropService.java
@@ -0,0 +1,53 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Crop;
+import com.betrybe.agrix.models.repositories.CropRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CropService {
+  private final CropRepository cropRepository;
+
+  @Autowired
+  public CropService(CropRepository cropRepository) {
+    this.cropRepository = cropRepository;
+  }
+
+  public Crop insertCrop(Crop crop) {
+    return cropRepository.save(crop);
+  }
+
+  public Optional<Crop> updateCrop(Integer id, Crop crop) {
+    Optional<Crop> optionalCrop = cropRepository.findById(id);
+
+    if (optionalCrop.isPresent()) {
+      Crop cropFromDB = optionalCrop.get();
+      cropFromDB.setName(crop.getName());
+
+      Crop updatedCrop = cropRepository.save(cropFromDB);
+      return Optional.of(updatedCrop);
+    }
+    return optionalCrop;
+  }
+
+  public Optional<Crop> removeCropById(Integer id) {
+    Optional<Crop> cropOptional = cropRepository.findById(id);
+
+    if(cropOptional.isPresent()) {
+      cropRepository.deleteById(id);
+    }
+    return cropOptional;
+  }
+
+  public Optional<Crop> getCropById(Integer id) {
+    return cropRepository.findById(id);
+  }
+
+  public List<Crop> getAllCrops() {
+    return cropRepository.findAll();
+  }
+}
---
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -0,0 +1,53 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.models.repositories.FarmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FarmService {
+  private final FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  public Farm insertFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+
+  public Optional<Farm> updateFarm(Integer id, Farm farm) {
+    Optional<Farm> optionalFarm = farmRepository.findById(id);
+
+    if (optionalFarm.isPresent()) {
+      Farm farmFromDB = optionalFarm.get();
+      farmFromDB.setName(farm.getName());
+
+      Farm updatedFarm = farmRepository.save(farmFromDB);
+      return Optional.of(updatedFarm);
+    }
+    return optionalFarm;
+  }
+
+  public Optional<Farm> removeFarmById(Integer id) {
+    Optional<Farm> farmOptional = farmRepository.findById(id);
+
+    if(farmOptional.isPresent()) {
+      farmRepository.deleteById(id);
+    }
+    return farmOptional;
+  }
+
+  public Optional<Farm> getFarmById(Integer id) {
+    return farmRepository.findById(id);
+  }
+
+  public List<Farm> getAllFarms() {
+    return farmRepository.findAll();
+  }
+}
