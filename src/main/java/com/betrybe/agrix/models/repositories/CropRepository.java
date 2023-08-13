784eeea408b368d010da61ca3520c078777a9811
Bete.mrt <bete.mrt@gmail.com>
2023-08-13 17:46:59 -0300

    repositories

---
+++ src/main/java/com/betrybe/agrix/models/repositories/CropRepository.java
@@ -0,0 +1,9 @@
+package com.betrybe.agrix.models.repositories;
+
+import com.betrybe.agrix.models.entities.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropRepository extends JpaRepository<Crop, Integer> {
+}
---
+++ src/main/java/com/betrybe/agrix/models/repositories/FarmRepository.java
@@ -0,0 +1,9 @@
+package com.betrybe.agrix.models.repositories;
+
+import com.betrybe.agrix.models.entities.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FarmRepository extends JpaRepository<Farm, Integer> {
+}
